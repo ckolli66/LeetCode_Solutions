bb22601f07b8ef45a6ddb0e65700af6b692de874
Chanakya Raghavendra <125328116+ckolli66@users.noreply.github.com>
2025-12-23 16:13:19 -0600

    Time: 3 ms (80.21%), Space: 18.4 MB (98.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,9 +7,7 @@ class Solution:
         j=0
         for i in range(0,n):
             if nums[i] != 0:
-                nums[j]= nums[i]
+                nums[j],nums[i]= nums[i],nums[j]
                 j+=1
-        for k in range(j,n):
-            nums[k]=0
         
         
