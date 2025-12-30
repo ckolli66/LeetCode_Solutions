da7d41d718e3d77e031f17dcaaa5825e535f33c7
Chanakya Raghavendra <125328116+ckolli66@users.noreply.github.com>
2025-12-30 16:49:16 -0600

    Time: 1 ms (79.94%), Space: 17.8 MB (92.02%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,23 +3,37 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m=len(matrix)
-        n=len(matrix[0])
-        rows=[]
-        columns=[]
-        for i in range(0,m):
-            for j in range(0,n):
+        m, n = len(matrix), len(matrix[0])
+        rows, cols = set(), set()
+
+        for i in range(m):
+            for j in range(n):
                 if matrix[i][j] == 0:
-                        rows.append(i)
-                        columns.append(j)
-        for i in range(0,len(rows)):
-            self.makezeroMatrix(rows[i],columns[i],m,n,matrix)
-    def makezeroMatrix(self,row :int,column :int,rowLength :int,columnLength :int,matrix: List[List[int]]) -> None:
-        for i in range(0,rowLength):
-            for j in range(0,columnLength):
-                if i==row:
-                    matrix[i][j]=0
-                if j==column:
-                    matrix[i][j]=0
+                    rows.add(i)
+                    cols.add(j)
+
+        for r in rows:
+            for j in range(n):
+                matrix[r][j] = 0
+
+        for c in cols:
+            for i in range(m):
+                matrix[i][c] = 0
+    #     m=len(matrix)
+    #     n=len(matrix[0])
+    #     rows=[]
+    #     columns=[]
+    #     for i in range(0,m):
+    #         for j in range(0,n):
+    #             if matrix[i][j] == 0:
+    #                 rows.append(i)
+    #                 columns.append(j)
+    #     for i in range(0,len(rows)):
+    #         self.makezeroMatrix(rows[i],columns[i],m,n,matrix)
+    # def makezeroMatrix(self,row :int,column :int,rowLength :int,columnLength :int,matrix: List[List[int]]) -> None:
+    #     for i in range(columnLength):
+    #         matrix[row][i]=0
+    #     for j in range(rowLength):
+    #         matrix[j][column]=0
                                        
 
